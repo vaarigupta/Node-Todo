05322239168a5b0492d5d6c94ede3cc134db1f00
vaarigupta <vaarigupta24@gmail.com>
2018-10-20 21:49:53 +0530

    mongoose added

---
+++ server/server.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+
+ /// used to build a connection to the database server 
+mongoose.Promise = global.Promise;
+const URL = "mongodb://localhost:27017/TodoApp_Mongoose";
+mongoose.connect(URL);
+
+//mongoose.model is used to create a blueprint of the document like class which gives the bluepeint of the object
+//Here we define the fields whicha we are going to have and define some of its characteristics
+var Todo = mongoose.model('Todo',{
+	text:{
+      type: String
+	},
+	completed : {
+      type : Boolean
+	},
+	completedAt:
+	{
+      type : Number
+	}
+})
+
+// here we are creating instance - creating a document in a Todo collection 
+var newTodo = new Todo({
+	text: "Raavi is a idiot girl"
+});
+
+var myTodo = new Todo({
+	text : "Vaari is a cute girl",
+	completed : true,
+	completedAt : 12
+})
+
+// saving the document which returns a promise bcz we have not passed a callback function in it
+// so we attach callback functions  -success and failure ,using then() method
+newTodo.save().then((doc)=>{
+	console.log("Saved Todo " , doc)
+
+},(err)=>{
+	console.log("Unable to save Todo")
+
+})
+
+myTodo.save().then((res)=>{
+	console.log("Result out : " , res)
+
+}, (err)=>{
+	 console.log("Unable to save Todo")
+	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
