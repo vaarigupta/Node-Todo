abe2b763bb81eeb8b8dafaf21da360ef61c49d96
vaarigupta <vaarigupta24@gmail.com>
2018-10-21 02:42:46 +0530

    refactoring of server folder and post request used

--- server/server.js
+++ server/server.js
@@ -7,21 +7,56 @@ var bodyParser = require('body-parser');
 var app = express();
 
 
-app.get('/',(req,res)=>{
-	if(req)
-	{
-		res.send('<h1> Welcome to my Todo App -Thanks for visiting </h1>')
-	}
+// app.get('/',(req,res)=>{
+// 	if(req)
+// 	{
+// 		res.send('<h1> Welcome to my Todo App -Thanks for visiting </h1>')
+// 	}
+
+// })
+// app.get('/todos',(req , res)=>{
+// 	if(req)
+// 	{
+// 		res.send("<h2>Done</h2>")
+// 	}
+
+
+// })
+app.use(bodyParser.json())
+app.post('/todos',(req , res)=>{
+
+	var newTodo = new Todo({
+		text : req.body.text
+	})
+
+	newTodo.save().then((doc)=>{
+		res.send(doc);
+
+	},(e)=>{
+
+		
+		res.status(400).send(e);
+
+	})
 
 })
-app.get('/todos',(req , res)=>{
-	if(req)
-	{
-		res.send("<h2>Done</h2>")
-	}
 
+app.post('/users',(req,res)=>{
+
+var newUser = new User({
+	email : req.body.email
+})
+
+newUser.save().then((doc)=>{
+	res.send(doc);
+
+},(e)=>{
+	res.status(400).send(e)
+})
 
 })
+
+
 
 app.listen(3000 , ()=>{
 	console.log("Running on port 3000");
