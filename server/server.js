68f97bba711cc27a7e615f3431595f3a679886e5
vaarigupta <vaarigupta24@gmail.com>
2018-10-21 19:26:45 +0530

    GET route done

--- server/server.js
+++ server/server.js
@@ -7,26 +7,18 @@ var bodyParser = require('body-parser');
 var app = express();
 
 
-// app.get('/',(req,res)=>{
-// 	if(req)
-// 	{
-// 		res.send('<h1> Welcome to my Todo App -Thanks for visiting </h1>')
-// 	}
 
-// })
-// app.get('/todos',(req , res)=>{
-// 	if(req)
-// 	{
-// 		res.send("<h2>Done</h2>")
-// 	}
-
-
-// })
 app.use(bodyParser.json())
-app.post('/todos',(req , res)=>{
 
+//------------------------------------------------------------------------------
+//POST - route
+app.post('/todos',(req , res)=>{
+     
+     var body = req.body;
 	var newTodo = new Todo({
-		text : req.body.text
+		text : body.text,
+		completed: body.completed,
+		completedAt : body.completedAt
 	})
 
 	newTodo.save().then((doc)=>{
@@ -51,8 +43,29 @@ newUser.save().then((doc)=>{
 	res.send(doc);
 
 },(e)=>{
-	res.status(400).send(e)
+	res.status(400).send(e);
 })
+
+})
+//----------------------------------------------------------------------------------------
+//GET - Route
+
+// app.get('/',(req,res)=>{
+// 	if(req)
+// 	{
+// 		res.send('<h1> Welcome to my Todo App -Thanks for visiting </h1>')
+// 	}
+
+// })
+app.get('/todos',(req , res)=>{
+	
+	Todo.find().then((todos)=>{
+		res.send({todos})
+
+	},(e)=>{
+		res.status(400).send(e)
+	})
+
 
 })
 
