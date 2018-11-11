fd0513d2a7e4ad9ec07eaadd421c50f06c9def58
vaarigupta <vaarigupta24@gmail.com>
2018-11-11 18:23:16 +0000

    modified

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
  /// used to build a connection to the database server 
 mongoose.Promise = global.Promise;
 const URL = "mongodb://localhost:27017/TodoApp_Mongoose";
-mongoose.connect("mongodb://<vaarigupta>:<vaari6029>@ds243798.mlab.com:43798/nodeapp"  || URL , {useNewUrlParser : true });
+mongoose.connect( URL , {useNewUrlParser : true });
 
 
 module.exports = {
