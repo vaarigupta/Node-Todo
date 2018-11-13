15b916f39df376082635fc72acd0e9639eb3d3e2
vaarigupta <vaarigupta24@gmail.com>
2018-11-14 05:17:51 +0530

    mlab hosted mongodb db added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
  /// used to build a connection to the database server 
 mongoose.Promise = global.Promise;
-const URL ="mongodb://admin:admin123.mlab.com:43798/nodeapp";
+const URL = "mongodb://admin:admin123@ds243798.mlab.com:43798/nodeapp";
 mongoose.connect( URL , {useNewUrlParser : true });
 
 
@@ -10,4 +10,4 @@ module.exports = {
 	mongoose
 }
 
-//"mongodb://localhost:27017/TodoApp_Mongoose"
+//mongodb://localhost:27017/TodoApp_Mongoose
