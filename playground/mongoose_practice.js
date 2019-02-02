bd0b832a07f11d8b1bcd782ab1ddea7490335ee3
vaarigupta <vaarigupta24@gmail.com>
2019-02-02 14:37:04 +0530

    Practice of get n post req

--- playground/mongoose_practice.js
+++ playground/mongoose_practice.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const express = require('express');
-
+const {ObjectID} = require('mongodb');
 
 mongoose.Promise = global.Promise;
  let Url = "mongodb://localhost:27017/TodoApp";
@@ -72,6 +72,12 @@ var id = "5c5449204d69743c4183e071";
 var id1 = "5c544a275247a43cf79f55b0";
 ///fetching Data from the Server
 app.get('/todos',(req,res)=>{
+
+  Todo.find().then((todos)=>{
+    res.send(todos);
+  },(e)=>{
+    res.status(400).send("unbble to fetch Data");
+  })
   // Todo.findById(id1).then((todo)=>{
   //   res.send({
   //     todo
@@ -85,17 +91,39 @@ app.get('/todos',(req,res)=>{
 /// method i.e findById() will be called for a request which will go to server side and finds the id thats of correct match
 /// and returns to the client then findOne() will never get called bcz for that a request was never made
 
+  // Todo.findOne({
+  //   _id: id
+  // }).then((todo)=>{
+  //   res.send({
+  //     todo
+  //   })
+  // }).catch((e)=>{
+  //   res.status(400).send("Sorry Unable to Fetch");
+  // })
 
-  Todo.findOne({
-    _id: id
-  }).then((todo)=>{
-    res.send({
-      todo
-    })
-  }).catch((e)=>{
-    res.status(400).send("Sorry Unable to Fetch");
-  })
 });
+
+app.get('/todos/:id',(req,res)=>{
+
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id))
+  {
+    return res.status(400).send("Id Is not Valid ");
+  }
+
+  Todo.findById(id).then((todo)=>{
+
+    var ans = todo? res.send(todo) : res.status(404).send({});
+    // var ans = todo? todo: {};
+    // res.send(ans);
+
+  },(e)=>{
+    res.status(404).send("Not Found any Todo");
+  })
+
+})
+
 
 app.listen(3000,()=>{
   console.log("Server running at port 3000");
